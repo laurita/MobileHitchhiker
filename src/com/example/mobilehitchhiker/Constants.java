85b4a941d29f1fe7badec3b659f488cc7d16cd1e
Laura Bledaite <laura.bledaite@gmail.com>
2012-12-19 07:43:09 +0100

    refactor

--- src/com/example/mobilehitchhiker/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.mobilehitchhiker;
-
-public class Constants {
-
-    public static final String LOGTAG = "MobileHitchhiker";
-	public static final String INTENT_ACTION_SHOW_TRIP = "com.example.mobilehitchhiker.SHOW_TRIP";
-	public static final String INTENT_ACTION_CREATE = "com.example.mobilehitchhiker.CREATE";
-	public static final String INTENT_ACTION_FIND = "com.example.mobilehitchhiker.FIND";
-}
